4cbedad0f5d0c7dfb81be05951accfdd6babfce7
geometriccross <geometriccross@gmail.com>
2024-09-27 16:47:18 +0000

    change a file

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import qiime2
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 import uvicorn
 
+templates = Jinja2Templates(directory="templates")
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/qiime2")
